1ea219a13f1a6449cc7762e3558e72495507a4b6
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-23 10:59:20 +0100

    testInstruction(), testAddInstructionStringString(), testAddInstructionStringIntIntInt() pass.

--- src/smlTests/AddInstructionTest.java
+++ src/smlTests/AddInstructionTest.java
@@ -13,24 +13,34 @@ import static org.mockito.Mockito.*;
 public class AddInstructionTest {
 
 //	Instruction instruction;
-	AddInstruction instruction;
+	AddInstruction instructionTest;
+	int op1Test = 28;
+	int op2Test = 10;
+	int resultTest = 3;
+	String labelTest = "labTest";
+	String opcodeTest = "add";
 	
 	@Before
 	public void setUp() throws Exception {
 	
-		instruction = new AddInstruction("test",1,2,3);
+		instructionTest = new AddInstruction(labelTest,resultTest,op1Test,op2Test);
 		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+	
+		instructionTest = null;
+	
 	}
 
 	@Test
 	public void testToString() {
 		
-		String expectedOutput = instruction.label + ": " + instruction.opcode  + " " + instruction.op1 + " + " + instruction.op2 + " to " + instruction.result;
-		String actualOutput = instruction.toString();
+//		String expectedOutput = instruction.label + ": " + instruction.opcode  + " " + instruction.op1 + " + " + instruction.op2 + " to " + instruction.result;
+		String expectedOutput = labelTest + ": " + opcodeTest  + " " + op1Test + " + " + op2Test + " to " + resultTest;
+		String actualOutput = instructionTest.toString();
+		System.out.println(actualOutput);
 		assertEquals(expectedOutput,actualOutput);
 	}
 
@@ -41,18 +51,33 @@ public class AddInstructionTest {
 
 	@Test
 	public void testAddInstructionStringString() {
-		fail("Not yet implemented");
+
+		instructionTest = new AddInstruction("labTest2", "bypassing2ndAddConstructor");
+		String expectedOutput = "labTest2" + "bypassing2ndAddConstructor";
+		String actualOutput = instructionTest.label + instructionTest.opcode;
+		assertEquals(expectedOutput,actualOutput);
+
 	}
 
+	/**
+	 * Test that constructor assigns member fields of Instruction and AddInstruction correctly 
+	 */
 	@Test
 	public void testAddInstructionStringIntIntInt() {
-		fail("Not yet implemented");
+		
+		String expectedOutput = labelTest+op2Test+opcodeTest+resultTest+op1Test;
+		String actualOutput = instructionTest.label+instructionTest.op2+instructionTest.opcode+instructionTest.result+instructionTest.op1;
+		assertEquals(expectedOutput,actualOutput);
+
 	}
 
 	@Test
 	public void testInstruction() {
 		
-		;
+		String expectedOutput = labelTest + opcodeTest;
+		String actualOutput = instructionTest.label + instructionTest.opcode;
+		assertEquals(expectedOutput,actualOutput);
+
 	}
 
 }
