ef7c5ad8ae8e2aee1c8c1199506ad2382d39f24b
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-23 14:22:37 +0100

    testExecute() completed, passes. AddInstructionTest completed. (Unfortunately did not use Mockito.)

--- src/smlTests/AddInstructionTest.java
+++ src/smlTests/AddInstructionTest.java
@@ -7,18 +7,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sml.AddInstruction;
-import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
 import static org.mockito.Mockito.*;
 
 public class AddInstructionTest {
 
-//	Instruction instruction;
 	AddInstruction instructionTest;
-	int op1Test = 28;
-	int op2Test = 10;
-	int resultTest = 3;
+	int op1Test = 28;   // the register location of one int value to be added. The value is 96 (see execute(Machine))
+	int op2Test = 10;   // the register location of the other int value to be added.The value is 69 (see execute(Machine))
+	int resultTest = 3; // the register location for the sum of the two int values. The value is 165.
 	String labelTest = "labTest";
 	String opcodeTest = "add";
+	int op1TestValue = 96;
+	int op2TestValue = 69;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -37,16 +39,45 @@ public class AddInstructionTest {
 	@Test
 	public void testToString() {
 		
-//		String expectedOutput = instruction.label + ": " + instruction.opcode  + " " + instruction.op1 + " + " + instruction.op2 + " to " + instruction.result;
 		String expectedOutput = labelTest + ": " + opcodeTest  + " " + op1Test + " + " + op2Test + " to " + resultTest;
 		String actualOutput = instructionTest.toString();
-		System.out.println(actualOutput);
 		assertEquals(expectedOutput,actualOutput);
+		
 	}
 
+	/**
+	 * I wasn't confident enough with Mocking to use it here.
+	 * Thus, the test relies on correct construction of a Machine object as well as 
+	 * a correct execute() method in Machine. These, as well as Registers getters and setters
+	 * have already been tested and shown to pass. Therefore I am happy to use them to test 
+	 * Instruction.  
+	 * 
+	 * The function of AddInstruction's execute(Machine) method is to get the two values 
+	 * from the two specified register positions, calculate their sum and then assign the sum 
+	 * to the specified register position.
+	 * So, I set up the machine's registers and put the two values at known positions. 
+	 * 
+	 * Next I run AddInstruction's execute(Machine) method and check for the correct sum
+	 * at the correct position in the register. 
+	 * 
+	 */
 	@Test
 	public void testExecute() {
-		fail("Not yet implemented");
+
+		Machine machineTest = new Machine();
+		machineTest.execute();//sets up empty register array
+		machineTest.getRegisters().setRegister(op1Test,op1TestValue);//sets the two values at the positions
+		machineTest.getRegisters().setRegister(op2Test,op2TestValue);//sets the two values at the positions
+		//AddInstruction has already assigned the 3 register positions to its member fields in the constructor call 
+		//in @Before setUp() above. So it knows where to look for the values and where to put the sum.
+		// now I just run the method
+		instructionTest.execute(machineTest);
+		//and look for the correct result in the correct position
+		int actualOutput = machineTest.getRegisters().getRegister(resultTest);
+		int expectedOutput = op1TestValue+op2TestValue;
+		
+		assertEquals(expectedOutput,actualOutput);
+	
 	}
 
 	@Test
@@ -79,5 +110,7 @@ public class AddInstructionTest {
 		assertEquals(expectedOutput,actualOutput);
 
 	}
+
+//	@Test(expected = ...Exception.class)
 
 }
